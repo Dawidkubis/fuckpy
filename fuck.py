1de0b109daefd8e62b41ac60354b01ae9d9537b2
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-10-03 22:50:19 +0200

    fuck.py almost done

--- fuck.py
+++ fuck.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
 
 from sys import argv
-import interactive
-import parse
+from os import path
 
+assert len(sys.argv) < 3, f'invalid number of arguments: {len(sys.argv)}'
 
+if len(sys.arv == 1):
+    import interactive
+else:
+    if path.exists(sys.argv[1]) and path.isfile(sys.argv[1]):
+        with open(sys.argv[1], 'r') as f:
+            from parse import parse
+            print(parse(f))
+    else:
+        raise AssertionError('problem opening file: {sys.argv[1]}')
